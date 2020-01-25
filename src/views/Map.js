61d7df9de4e3d2e18ebdbf61506d18e88e13785d
Danil <front.end.poznyakov@gmail.com>
2020-01-25 14:36:42 +0200

    added mapbox

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
+import '../scss/Map.scss';
+
 
 const Maps = ({ setPage }) => {
+    var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+    let mapContainer = React.createRef();
+
+    mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWwwMDUyNCIsImEiOiJjazV0amd0NXYwNjA5M2VvYTE1ZmU4dThyIn0.Zn65a2N1HP929vgwZeimNQ';
+
+    useEffect(() => {
+        mapContainer = new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-74.5, 40],
+            zoom: 9,
+        });
+    }, []);
+
     return (
         <section className='map'>
             <Header setPage={setPage} />
-            <div className='container'>
-            </div>
+            <div id="map" ref={el => mapContainer = el}></div>
         </section>
     );
 }
