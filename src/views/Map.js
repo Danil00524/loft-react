ea318a559a7424e0cf7b9404c7c4e86b51611226
Danil <front.end.poznyakov@gmail.com>
2020-01-26 20:32:31 +0200

    delete comment

--- src/views/Map.js
+++ src/views/Map.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-// import mapboxgl from 'mapbox-gl';
+import mapboxgl from 'mapbox-gl';
 
 import Header from '../components/Header';
 // import LoginContext from '../context/Login';
@@ -9,19 +9,19 @@ import Header from '../components/Header';
 import '../scss/Map.scss';
 import { mapbox } from '../constants/tokens';
 
-// mapboxgl.accessToken = mapbox;
+mapboxgl.accessToken = mapbox;
 
 const Maps = ({ setPage }) => {
     let mapContainer = React.createRef();
 
-    // useEffect(() => {
-    //     new mapboxgl.Map({
-    //         container: mapContainer,
-    //         style: 'mapbox://styles/mapbox/streets-v11',
-    //         center: [-74.5, 40],
-    //         zoom: 9,
-    //     });
-    // }, [mapContainer]);
+    useEffect(() => {
+        new mapboxgl.Map({
+            container: mapContainer,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [-74.5, 40],
+            zoom: 9,
+        });
+    }, [mapContainer]);
 
     return (
         <section className='map'>
