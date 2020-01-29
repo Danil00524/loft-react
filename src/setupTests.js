956adc7e2bc5a8f888a6667b74f4bd1733f9f235
Danil <front.end.poznyakov@gmail.com>
2020-01-29 21:51:59 +0200

    added mock map

--- src/setupTests.js
+++ src/setupTests.js
@@ -6,4 +6,17 @@ import '@testing-library/jest-dom/extend-expect';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+    GeolocateControl: jest.fn(),
+    Map: jest.fn(() => ({
+        addControl: jest.fn(),
+        on: jest.fn(),
+        remove: jest.fn()
+    })),
+    NavigationControl: jest.fn()
+}));
+
+export default undefined;
+
 configure({ adapter: new Adapter() });
