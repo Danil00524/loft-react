daa5c9fa466c64bc4421a182fa63d373cb6434dc
Danil <front.end.poznyakov@gmail.com>
2020-05-03 23:22:54 +0300

    added build

---
+++ build/precache-manifest.bedadc856998790543eaf640e45f53d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23a8883b0819155d771a202088c51120",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ecc99d6d347d168385e",
+    "url": "/static/css/main.29c8bb35.chunk.css"
+  },
+  {
+    "revision": "3ce90c749c4d8d481a1c",
+    "url": "/static/js/2.cb5698ca.chunk.js"
+  },
+  {
+    "revision": "3d416322b0ec4a5a91d5be4afe7c848c",
+    "url": "/static/js/2.cb5698ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5ecc99d6d347d168385e",
+    "url": "/static/js/main.60dca773.chunk.js"
+  },
+  {
+    "revision": "ba0713d26f27847484cc",
+    "url": "/static/js/runtime-main.aea5b7ed.js"
+  },
+  {
+    "revision": "0cab89b32ec1a203d5d2f65ad898adef",
+    "url": "/static/media/фон.0cab89b3.jpg"
+  }
+]);
